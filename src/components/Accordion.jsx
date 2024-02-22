367857ac274af537b0d8e20ab9fea463f29f9011
tapaskulti <tapaskulti@gmail.com>
2024-02-22 16:09:29 +0530

    checkout page

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 // eslint-disable-next-line react/prop-types
-const Accordion = ({ element,onCheckChange,name}) => {
+const Accordion = ({ element, onCheckChange, name }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
   return (
     <div className="py-2">
@@ -46,11 +47,17 @@ const Accordion = ({ element,onCheckChange,name}) => {
         }`}
       >
         <div className="overflow-hidden">
-          {element[0]?.element?.map((singleElement,index) => {
+          {element[0]?.element?.map((singleElement, index) => {
             return (
               <div key={index}>
                 <div className="flex items-center space-x-2">
-                  <input type="checkbox" className="border border-slate-50" name={name} value={singleElement} onChange={onCheckChange}></input>
+                  <input
+                    type="checkbox"
+                    className="border border-slate-50"
+                    name={name}
+                    value={singleElement}
+                    onChange={onCheckChange}
+                  ></input>
                   <div className="my-0.5">{singleElement}</div>
                 </div>
               </div>
@@ -64,4 +71,100 @@ const Accordion = ({ element,onCheckChange,name}) => {
 
 export default Accordion;
 
-
+export const AccordionContinue = () => {
+  const { cartDetails } = useSelector((state) => state.cart);
+  return (
+    <div>
+      <div className="collapse collapse-arrow bg-white/90 border-b border-t border-slate-200 rounded-none my-2">
+        <input type="radio" name="my-accordion-2"/>
+        <div className="collapse collapse-title text-base font-medium">
+          Your Cart
+        </div>
+        <div className="collapse-content">
+        {cartDetails?.arts?.map((singleArt) => {
+                  return (
+                    <div className="space-y-3" key={singleArt?._id}>
+                      <div className=" bg-white border-t-2 flex p-3">
+                        <div className="w-[30%]">
+                          <img src={singleArt?.thumbnail?.secure_url} alt="" />
+                        </div>
+                        <div className="w-[70%] px-3 space-y-1">
+                          <div className="flex justify-between">
+                            <div>
+                              <h1 className="text-base italic">
+                                {singleArt?.title}
+                              </h1>
+                              <h1 className=" text-sm">Soo Beng Lim</h1>
+                              <h1 className=" text-xs">{singleArt?.subject}</h1>
+                            </div>
+                            <button
+                              className="bg-gray-300 text-white font-semibold rounded-full h-6 p-2 flex items-center"
+                              onClick={() => {
+                                dispatch({
+                                  type: "REMOVE_ART_FROM_CART",
+                                  payload: {
+                                    userId: authUser?._id,
+                                    artId: singleArt?._id,
+                                    artPrice: singleArt?.price,
+                                  },
+                                });
+                              }}
+                            >
+                              X
+                            </button>
+                          </div>
+                          <div className="flex justify-between text-xs">
+                            <h1>Shipping</h1>
+                            <h1>Included</h1>
+                          </div>
+                          <div className="flex justify-between text-sm font-semibold">
+                            <h1>Artwork Total</h1>
+                            <h1>{singleArt?.price}</h1>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-white/90 border-b border-t border-slate-200 rounded-none my-2">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-base font-medium">
+          Shipping Address
+        </div>
+        <div className="collapse-content">
+          <div className="space-y-5">
+            <div className="flex items-center space-x-5">
+              <input type="text" placeholder="First Name*" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+              <input type="text" placeholder="Last Name*" className="w-full border border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            </div>
+            <input type="text" placeholder="Address1*" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            <input type="text" placeholder="Address2" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            <div className="flex items-center space-x-5">
+              <input type="text" placeholder="Country*" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+              <input type="text" placeholder="City*" className="w-full border border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <input type="text" placeholder="State/Region*" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+              <input type="text" placeholder="Zip/Postal Code*" className="w-full border border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <input type="text" placeholder="Email*" className="w-full border text-slate-400 border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+              <input type="text" placeholder="Phone Number*" className="w-full border border-slate-200 rounded-md px-3 py-3 focus:border-none focus:outline-slate-400 focus:outline-1" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="collapse collapse-arrow bg-white/90 border-b border-t border-slate-200 rounded-none my-2">
+        <input type="radio" name="my-accordion-2" />
+        <div className="collapse-title text-base font-medium">
+          Payment Method
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/ShippingBilling.jsx
+++ src/pages/ShippingBilling.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
-import { HiOutlineShoppingCart } from "react-icons/hi2";
+
 import { IoShieldCheckmarkSharp } from "react-icons/io5";
 import { TiLockClosed } from "react-icons/ti";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { FaStar } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
+import { AccordionContinue } from "../components/Accordion";
 
 const Cart = () => {
   const { cartDetails } = useSelector((state) => state.cart);
@@ -17,79 +17,22 @@ const Cart = () => {
         <div className="flex items-center justify-between py-6 mx-5 md:mx-10 md:flex md:justify-between hover:text-primary">
           <div className="flex items-center justify-center md:justify-start">
             <Link to="/">
-              <img src={logo} className="h-6 " alt="Artkya Logo" />
+              {/* <img src={logo} className="h-6 " alt="Artkya Logo" /> */}
             </Link>
           </div>
         </div>
       </div>
       {/* *************************************************** */}
       <div className="my-10">
-        <div className="flex px-60 py-10 space-x-6">
-          <div className="w-3/5 bg-slate-50  p-4">
-            <div className="text-4xl font-semibold">Cart</div>
-            {cartDetails?.arts?.length === 0 ? (
-              <div className="flex flex-col items-center space-y-5">
-                <HiOutlineShoppingCart className="text-[16rem] text-slate-300" />
-                <h1 className="text-xl">Your Cart Is Empty</h1>
-                <Link
-                  to="/Painting"
-                  className={`flex cursor-pointer w-72 bg-black text-white font-semibold justify-center items-center py-4 text-2xl `}
-                >
-                  Browse Art
-                </Link>
-              </div>
-            ) : (
-              <>
-                {cartDetails?.arts?.map((singleArt) => {
-                  return (
-                    <div className="space-y-3" key={singleArt?._id}>
-                      <div className=" bg-white border-t-2 flex p-3">
-                        <div className="w-[30%]">
-                          <img src={singleArt?.thumbnail?.secure_url} alt="" />
-                        </div>
-                        <div className="w-[70%] px-3 space-y-1">
-                          <div className="flex justify-between">
-                            <div>
-                              <h1 className="text-base italic">
-                                {singleArt?.title}
-                              </h1>
-                              <h1 className=" text-sm">Soo Beng Lim</h1>
-                              <h1 className=" text-xs">{singleArt?.subject}</h1>
-                            </div>
-                            <button
-                              className="bg-gray-300 text-white font-semibold rounded-full h-6 p-2 flex items-center"
-                              onClick={() => {
-                                dispatch({
-                                  type: "REMOVE_ART_FROM_CART",
-                                  payload: {
-                                    userId: authUser?._id,
-                                    artId: singleArt?._id,
-                                    artPrice: singleArt?.price,
-                                  },
-                                });
-                              }}
-                            >
-                              X
-                            </button>
-                          </div>
-                          <div className="flex justify-between text-xs">
-                            <h1>Shipping</h1>
-                            <h1>Included</h1>
-                          </div>
-                          <div className="flex justify-between text-sm font-semibold">
-                            <h1>Artwork Total</h1>
-                            <h1>{singleArt?.price}</h1>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
-              </>
-            )}
+        <div className="flex justify-center px-64 py-10 space-x-16">
+          <div className="w-2/5 bg-slate-50  p-4">
+            <div className="text-4xl font-semibold">Checkout</div>
+            <div>
+              <AccordionContinue className="block" />
+            </div>
           </div>
           {/* ************************************************************************************************************** */}
-          <div className="w-2/5 bg-slate-50 p-4 space-y-4">
+          <div className="w-1/3 bg-slate-50 p-4 space-y-4">
             <div className="space-y-3">
               <div className="flex text-xl justify-between font-semibold">
                 <h1>Estimated Total</h1>
@@ -99,11 +42,9 @@ const Cart = () => {
                 All charges and refunds will be made in USD ($106,615) and may
                 be subject to exchange rate fluctuations.
               </h1>
-              <Link to="/ShippingBilling">
-                <button className="flex w-full bg-[#80bc30] hover:bg-[#89c043] text-white text-xl font-semibold mt-5 px-10 py-4 justify-center">
-                  Checkout
-                </button>
-              </Link>
+              <div className="flex bg-[#80bc30] text-white text-xl font-semibold px-10 py-4 justify-center">
+                Checkout
+              </div>
             </div>
             {/* ************************************************************************************************************** */}
             <div>
