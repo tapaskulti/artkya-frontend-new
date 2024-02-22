6cfe384072d40baea328f16c64867919dcc02bf9
RisuSingh777 <rishu@trivediat.com>
2024-02-22 23:40:26 +0530

    upload image

--- src/pages/artist_dashboard/ImageUploadForm.jsx
+++ src/pages/artist_dashboard/ImageUploadForm.jsx
@@ -1,59 +1,16 @@
+// ImageUploadForm component
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage, faUpload } from "@fortawesome/free-solid-svg-icons";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import Dropzone from "../../components/Dropzone";
 
 const ImageUploadForm = () => {
   const [images, setImages] = useState([]);
   const [title, setTitle] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
 
-  const handleImageChange = (e) => {
-    const newImages = [...images];
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const imageData = {
-        file: file,
-        src: event.target.result,
-        title: title,
-        size: file.size,
-        originalDimensions: `${file.width}x${file.height}`,
-      };
-      newImages.push(imageData);
-      setImages(newImages);
-    };
-
-    reader.readAsDataURL(file);
-  };
-
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const file = e.dataTransfer.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const imageData = {
-          file: file,
-          src: event.target.result,
-          title: title,
-          size: file.size,
-          originalDimensions: `${file.width}x${file.height}`,
-        };
-        const newImages = [...images];
-        newImages.push(imageData);
-        setImages(newImages);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
   };
 
   const openModal = () => {
@@ -66,7 +23,10 @@ const ImageUploadForm = () => {
 
   const handleImageSelection = (selectedImages) => {
     const newImages = [...images];
+    // newImages.push(selectedImages)
+    console.log('selectedImages',selectedImages);
     selectedImages.forEach((selectedImage) => {
+      console.log('selectedImage',selectedImage);
       newImages.push({
         file: selectedImage,
         src: URL.createObjectURL(selectedImage),
@@ -82,26 +42,17 @@ const ImageUploadForm = () => {
   return (
     <div className="flex w-full">
       <div className="w-1/3 p-4">
-        <div
-          className="preview-section border border-gray-300 rounded p-4 flex flex-wrap"
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-        >
+        <div className="preview-section border border-gray-300 rounded p-4 flex flex-wrap">
           {images.length === 0 && (
-            <FontAwesomeIcon
-              icon={faImage}
-              className="imageUploadPreviewIcon"
-            />
+            <FontAwesomeIcon icon={faImage} className="imageUploadPreviewIcon" />
           )}
           {images.length > 0 &&
             images.map((image, index) => (
               <div key={index} className="image-preview mb-4">
                 <img
                   src={image.src}
-                  alt={`Preview ${index}`}
-                  className={`max-w-full ${
-                    index === 0 ? "w-100 h-100" : "w-40 h-40"
-                  }`}
+                  alt={`Preview ${index+1}`}
+                  className={`max-w-full ${index === 0 ? "w-100 h-100" : "w-40 h-40"}`}
                 />
               </div>
             ))}
@@ -152,85 +103,8 @@ const ImageUploadForm = () => {
       </div>
 
       {modalOpen && (
-        <Modal onClose={closeModal} onImageSelection={handleImageSelection} />
+        <Dropzone closeModal={closeModal} handleImageSelection={handleImageSelection} />
       )}
-    </div>
-  );
-};
-
-const Modal = ({ onClose, onImageSelection }) => {
-  const [selectedImages, setSelectedImages] = useState([]);
-
-  const handleImageChange = (e) => {
-    const files = Array.from(e.target.files);
-    setSelectedImages(files);
-  };
-
-  const handleSubmit = () => {
-    onImageSelection(selectedImages);
-  };
-
-  return (
-    <div className="imageUploader">
-      <div className="innerImageUploader">
-        <h2 className="text-xl font-bold mb-4">Upload Image</h2>
-        <div className="dropzone-container">
-          <label className="dropzoneInput cursor-pointer">
-            <div className="dropzoneInputInner">
-              <FontAwesomeIcon
-                icon={faUpload}
-                className="text-gray-500 uploadIcon"
-              />
-              <p className="mt-2">Drag and drop image here</p>
-              <p className="mt-2">or<u> BROWSE IMAGE</u></p>
-              <input
-                id="modalImageInput"
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                multiple={true}
-                className="hidden"
-              />
-            </div>
-          </label>
-        </div>
-
-        <div className="flex imageUploaderFooter mt-6">
-          <div className="image-req-text">
-            <p>Your image file must be:</p>
-            <ul className="ul-list">
-              <li>
-                <i>JPEG or PNG format</i>
-              </li>
-              <li>
-                <i>At least 1500W x 1200H pixels</i>
-              </li>
-              <li>
-                <i>Less than 50MB</i>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <div className="flex justify-end mt-6">
-              <button className="text-blue-500 mr-4" onClick={onClose}>
-                Cancel
-              </button>
-              <button
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
-            </div>
-            <div className="flex justify-end mt-6">
-              <p className="text-sm text-gray-500 ">
-                By clicking "Submit", I confirm that I am the copyright owner of
-                this image.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
